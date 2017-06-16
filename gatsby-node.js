91720e96b2cc91dc1ff96b4859c7cc9655f465cc
Austin Lanari <austin.lanari@gmail.com>
2017-06-15 22:19:40 -0400

    add date to hollowing

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,7 +33,7 @@ exports.modifyWebpackConfig = function (config) {
   return config
 }
 
-function pagesToSitemap(pages) {
+function pagesToSitemap (pages) {
   var urls = pages.map(function(p) {
     if (p.path !== undefined) {
       return {
@@ -47,11 +47,11 @@ function pagesToSitemap(pages) {
   return urls.filter(function(u) { return u !== undefined})
 }
 
-function generateSiteMap(pages) {
+function generateSiteMap (pages) {
   var sitemap = sm.createSitemap({
     hostname: 'http://fuckupsomecomics.com',
     cacheTime: '60000',
-    urls: pagesToSitemap(pages),
+    urls: pagesToSitemap(pages)
   })
   console.log('Generating sitemap.xml')
   fs.writeFileSync(
