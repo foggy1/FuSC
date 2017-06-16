d4bcde2c9d84e9b9af422d2e59e0347bbb69c601
Austin Lanari <austin.lanari@gmail.com>
2017-06-15 23:28:22 -0400

    sort out share links

--- components/SitePost/index.jsx
+++ components/SitePost/index.jsx
@@ -34,24 +34,49 @@ class SitePost extends React.Component {
           <div className='footer'>
             <ReadNext post={post} {...this.props} />
             <hr />
-            <ul className="rrssb-buttons clearfix">
-              <li className="rrssb-email">
-                <a href="mailto:?Subject=your%20subject">
-                  <span className="rrssb-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><path d="M20.11 26.147c-2.335 1.05-4.36 1.4-7.124 1.4C6.524 27.548.84 22.916.84 15.284.84 7.343 6.602.45 15.4.45c6.854 0 11.8 4.7 11.8 11.252 0 5.684-3.193 9.265-7.398 9.3-1.83 0-3.153-.934-3.347-2.997h-.077c-1.208 1.986-2.96 2.997-5.023 2.997-2.532 0-4.36-1.868-4.36-5.062 0-4.75 3.503-9.07 9.11-9.07 1.713 0 3.7.4 4.6.972l-1.17 7.203c-.387 2.298-.115 3.3 1 3.4 1.674 0 3.774-2.102 3.774-6.58 0-5.06-3.27-8.994-9.304-8.994C9.05 2.87 3.83 7.545 3.83 14.97c0 6.5 4.2 10.2 10 10.202 1.987 0 4.09-.43 5.647-1.245l.634 2.22zM16.647 10.1c-.31-.078-.7-.155-1.207-.155-2.572 0-4.596 2.53-4.596 5.53 0 1.5.7 2.4 1.9 2.4 1.44 0 2.96-1.83 3.31-4.088l.592-3.72z"/></svg></span>
-                  <span className="rrssb-text">email</span>
+            <ul className='rrssb-buttons clearfix'>
+              <li className='rrssb-facebook'>
+                <a href='https://www.facebook.com/sharer/sharer.php?u=http://your-url-here' className='popup'>
+                  <span className='rrssb-icon'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 29 29'><path d='M26.4 0H2.6C1.714 0 0 1.715 0 2.6v23.8c0 .884 1.715 2.6 2.6 2.6h12.393V17.988h-3.996v-3.98h3.997v-3.062c0-3.746 2.835-5.97 6.177-5.97 1.6 0 2.444.173 2.845.226v3.792H21.18c-1.817 0-2.156.9-2.156 2.168v2.847h5.045l-.66 3.978h-4.386V29H26.4c.884 0 2.6-1.716 2.6-2.6V2.6c0-.885-1.716-2.6-2.6-2.6z' /></svg></span>
+                  <span className='rrssb-text'>facebook</span>
                 </a>
               </li>
-              <li className="rrssb-facebook">
-                <a href="https://www.facebook.com/sharer/sharer.php?u=http://your-url-here" className="popup">
-                  <span className="rrssb-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29 29"><path d="M26.4 0H2.6C1.714 0 0 1.715 0 2.6v23.8c0 .884 1.715 2.6 2.6 2.6h12.393V17.988h-3.996v-3.98h3.997v-3.062c0-3.746 2.835-5.97 6.177-5.97 1.6 0 2.444.173 2.845.226v3.792H21.18c-1.817 0-2.156.9-2.156 2.168v2.847h5.045l-.66 3.978h-4.386V29H26.4c.884 0 2.6-1.716 2.6-2.6V2.6c0-.885-1.716-2.6-2.6-2.6z"/></svg></span>
-                  <span className="rrssb-text">facebook</span>
+              <li className='rrssb-twitter'>
+                <a href='https://twitter.com/intent/tweet?text=http://rrssb.ml'
+                  className='popup'>
+                  <span className='rrssb-icon'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 28 28'><path d='M24.253 8.756C24.69 17.08 18.297 24.182 9.97 24.62a15.093 15.093 0 0 1-8.86-2.32c2.702.18 5.375-.648 7.507-2.32a5.417 5.417 0 0 1-4.49-3.64c.802.13 1.62.077 2.4-.154a5.416 5.416 0 0 1-4.412-5.11 5.43 5.43 0 0 0 2.168.387A5.416 5.416 0 0 1 2.89 4.498a15.09 15.09 0 0 0 10.913 5.573 5.185 5.185 0 0 1 3.434-6.48 5.18 5.18 0 0 1 5.546 1.682 9.076 9.076 0 0 0 3.33-1.317 5.038 5.038 0 0 1-2.4 2.942 9.068 9.068 0 0 0 3.02-.85 5.05 5.05 0 0 1-2.48 2.71z' /></svg></span>
+                  <span className='rrssb-text'>twitter</span>
                 </a>
               </li>
-              <li className="rrssb-twitter">
-                <a href="https://twitter.com/intent/tweet?text=http://rrssb.ml"
-                className="popup">
-                  <span className="rrssb-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><path d="M24.253 8.756C24.69 17.08 18.297 24.182 9.97 24.62a15.093 15.093 0 0 1-8.86-2.32c2.702.18 5.375-.648 7.507-2.32a5.417 5.417 0 0 1-4.49-3.64c.802.13 1.62.077 2.4-.154a5.416 5.416 0 0 1-4.412-5.11 5.43 5.43 0 0 0 2.168.387A5.416 5.416 0 0 1 2.89 4.498a15.09 15.09 0 0 0 10.913 5.573 5.185 5.185 0 0 1 3.434-6.48 5.18 5.18 0 0 1 5.546 1.682 9.076 9.076 0 0 0 3.33-1.317 5.038 5.038 0 0 1-2.4 2.942 9.068 9.068 0 0 0 3.02-.85 5.05 5.05 0 0 1-2.48 2.71z"/></svg></span>
-                  <span className="rrssb-text">twitter</span>
+              <li className='rrssb-tumblr'>
+                <a href='http://tumblr.com/share/link?url=http%3A%2F%2Fwww.kurtnoble.com%2Flabs%2Frrssb&name=Ridiculously%20Responsive%20Social%20Sharing%20Buttons%20by%20KNI%20Labs'>
+                  <span className='rrssb-icon'>
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 28 28'><path d='M18.02 21.842c-2.03.052-2.422-1.396-2.44-2.446v-7.294h4.73V7.874H15.6V1.592h-3.714s-.167.053-.182.186c-.218 1.935-1.144 5.33-4.988 6.688v3.637h2.927v7.677c0 2.8 1.7 6.7 7.3 6.6 1.863-.03 3.934-.795 4.392-1.453l-1.22-3.54c-.52.213-1.415.413-2.115.455z' /></svg>
+                  </span>
+                  <span className='rrssb-text'>tumblr</span>
+                </a>
+              </li>
+              <li className='rrssb-googleplus'>
+                <a href='https://plus.google.com/share?url=http://rrssb.ml' className='popup'>
+                  <span className='rrssb-icon'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M21 8.29h-1.95v2.6h-2.6v1.82h2.6v2.6H21v-2.6h2.6v-1.885H21V8.29zM7.614 10.306v2.925h3.9c-.26 1.69-1.755 2.925-3.9 2.925-2.34 0-4.29-2.016-4.29-4.354s1.885-4.353 4.29-4.353c1.104 0 2.014.326 2.794 1.105l2.08-2.08c-1.3-1.17-2.924-1.883-4.874-1.883C3.65 4.586.4 7.835.4 11.8s3.25 7.212 7.214 7.212c4.224 0 6.953-2.988 6.953-7.082 0-.52-.065-1.104-.13-1.624H7.614z' /></svg>                      </span>
+                  <span className='rrssb-text'>google+</span>
+                </a>
+              </li>
+              <li className='rrssb-pocket'>
+                <a href='https://getpocket.com/save?url=http://rrssb.ml'>
+                  <span className='rrssb-icon'>
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'><path d='M28.782.002c2.03.002 3.193 1.12 3.182 3.106-.022 3.57.17 7.16-.158 10.7-1.09 11.773-14.588 18.092-24.6 11.573C2.72 22.458.197 18.313.057 12.937c-.09-3.36-.05-6.72-.026-10.08C.04 1.113 1.212.016 3.02.008 7.347-.006 11.678.004 16.006.002c4.258 0 8.518-.004 12.776 0zM8.65 7.856c-1.262.135-1.99.57-2.357 1.476-.392.965-.115 1.81.606 2.496a746.818 746.818 0 0 0 7.398 6.966c1.086 1.003 2.237.99 3.314-.013a700.448 700.448 0 0 0 7.17-6.747c1.203-1.148 1.32-2.468.365-3.426-1.01-1.014-2.302-.933-3.558.245-1.596 1.497-3.222 2.965-4.75 4.526-.706.715-1.12.627-1.783-.034a123.71 123.71 0 0 0-4.93-4.644c-.47-.42-1.123-.647-1.478-.844z' /></svg>
+                  </span>
+                  <span className='rrssb-text'>pocket</span>
+                </a>
+              </li>
+              <li className='rrssb-whatsapp'>
+                <a href='whatsapp://send?text=Ridiculously%20Responsive%20Social%20Sharing%20Buttons%3A%20http%3A%2F%2Frrssb.ml%20%7C%20http%3A%2F%2Frrssb.ml%2Frrssb-preview.png' data-action='share/whatsapp/share'>
+                  <span className='rrssb-icon'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='90' height='90' viewBox='0 0 90 90'><path d='M90 43.84c0 24.214-19.78 43.842-44.182 43.842a44.256 44.256 0 0 1-21.357-5.455L0 90l7.975-23.522a43.38 43.38 0 0 1-6.34-22.637C1.635 19.63 21.415 0 45.818 0 70.223 0 90 19.628 90 43.84zM45.818 6.983c-20.484 0-37.146 16.535-37.146 36.86 0 8.064 2.63 15.533 7.076 21.61l-4.64 13.688 14.274-4.537A37.122 37.122 0 0 0 45.82 80.7c20.48 0 37.145-16.533 37.145-36.857S66.3 6.983 45.818 6.983zm22.31 46.956c-.272-.447-.993-.717-2.075-1.254-1.084-.537-6.41-3.138-7.4-3.495-.993-.36-1.717-.54-2.438.536-.72 1.076-2.797 3.495-3.43 4.212-.632.72-1.263.81-2.347.27-1.082-.536-4.57-1.672-8.708-5.332-3.22-2.848-5.393-6.364-6.025-7.44-.63-1.076-.066-1.657.475-2.192.488-.482 1.084-1.255 1.625-1.882.543-.628.723-1.075 1.082-1.793.363-.718.182-1.345-.09-1.884-.27-.537-2.438-5.825-3.34-7.977-.902-2.15-1.803-1.793-2.436-1.793-.63 0-1.353-.09-2.075-.09-.722 0-1.896.27-2.89 1.344-.99 1.077-3.788 3.677-3.788 8.964 0 5.288 3.88 10.397 4.422 11.113.54.716 7.49 11.92 18.5 16.223 11.01 4.3 11.01 2.866 12.996 2.686 1.984-.18 6.406-2.6 7.312-5.107.9-2.513.9-4.664.63-5.112z' /></svg>
+                  </span>
+                  <span className='rrssb-text'>Whatsapp</span>
                 </a>
               </li>
             </ul>
@@ -62,18 +87,18 @@ class SitePost extends React.Component {
             </p>
           </div>
         </div>
-        <meta property="og:url" content={prefixLink(post.path)} />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content={post.title} />
-        <meta property="og:description" content={post.description} />
-        <meta name="twitter:site" value="@austinlanari" />
-        <meta property="twitter:url" content={prefixLink(post.path)} />
-        <meta property="twitter:title" content={post.title} />
-        <meta property="twitter:description" content={post.description} />
-        <meta name="author" content="Austin Lanari" />
-        <meta name="og:image" content={post.indexImage} />
-        <meta name="twitter:image" content={post.indexImage} />
-        <meta name="twitter:card" value="summary_large_image" />
+        <meta property='og:url' content={prefixLink(post.path)} />
+        <meta property='og:type' content='article' />
+        <meta property='og:title' content={post.title} />
+        <meta property='og:description' content={post.description} />
+        <meta name='twitter:site' value='@austinlanari' />
+        <meta property='twitter:url' content={prefixLink(post.path)} />
+        <meta property='twitter:title' content={post.title} />
+        <meta property='twitter:description' content={post.description} />
+        <meta name='author' content='Austin Lanari' />
+        <meta name='og:image' content={post.indexImage} />
+        <meta name='twitter:image' content={post.indexImage} />
+        <meta name='twitter:card' value='summary_large_image' />
       </div>
     )
   }
