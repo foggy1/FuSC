959a79ffd1b12ced758cfb3131d3deb962b10322
Austin Lanari <austin.lanari@gmail.com>
2017-06-19 22:31:52 -0400

    deprecate config toml

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Fuck Up Some Comics`,
+    description: `Fuck up some comics, yeah`,
+    author: 'Foggy',
+    twitter: `https://www.twitter.com/austinlanari`,
+    github: `https://www.github.com/foggy1`,
+    rss: `http://fuckupsomecomics.com/rss.xml`,
+    atom: `http://fuckupsomecomics.com/atom.xml`,
+    googleAnalytics: `UA-80918633-2`
+  }
+}
