2ea5026bde86374a3ac8dcb683f1ad166c1cb4a0
Austin Lanari <austin.lanari@gmail.com>
2017-06-25 12:04:45 -0400

    barebones config

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteMetadata: {
+    title: "Fuck Up Some Comics",
+    author: "Austin Lanari",
+    
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-responsive-image`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-80918633-2`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+}
