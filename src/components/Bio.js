0cc2219f0ffac48ceef81a039e5e68400a9393da
Austin Lanari <austin.lanari@gmail.com>
2017-06-25 12:34:20 -0400

    layouts

---
+++ src/components/Bio.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+// Import typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+
+import profilePic from "./profile-pic.jpg"
+import { rhythm } from "../utils/typography"
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            float: "left",
+            marginRight: rhythm(1 / 4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        Written by
+        {" "}
+        <strong>Austin Lanari</strong>
+        {" "}
+        who builds and writes cool stuff as much as he can.
+        {" "}
+        <a href="https://twitter.com/austinlanari">
+          I tweet
+        </a>
+      </p>
+    )
+  }
+}
+
+export default Bio
