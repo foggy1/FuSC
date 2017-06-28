a8b09f0864bd31c984c11ab179bbb538cd4c0910
Austin Lanari <austin.lanari@gmail.com>
2017-06-27 22:44:09 -0400

    add parnassus typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import Parnassus from 'typography-theme-parnassus'
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Parnassus)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
